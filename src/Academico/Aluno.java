f81ae4f4ffaad50df4149363fe837fac5c8ad510
namomalencar <namomalencar@gmail.com>
2016-07-28 19:29:15 -0300

    Adição dos Slides

--- src/Academico/Aluno.java
+++ src/Academico/Aluno.java
@@ -50,22 +50,22 @@ public class Aluno {
 		float media = calcMedia();
 		if (media >= 7) {
 			System.out.println("O aluno " + nome + " foi aprovado com a media: " + media);
-			System.exit(0);
+//			System.exit(0);
 		}
 
 		if (media >= 5) {
 			System.out.println("O aluno " + nome + " ficou de recuperação com a media: " + media);
-			System.exit(0);
+//			System.exit(0);
 		}
 
 		if (media >= 4) {
 			System.out.println("O aluno " + nome + " ficou de recuperação + trabalho com a media: " + media);
-			System.exit(0);
+//			System.exit(0);
 		}
 
 		if (media < 4) {
 			System.out.println("O aluno " + nome + " foi reprovado com a media: " + media);
-			System.exit(0);
+//			System.exit(0);
 		}
 	}
 
--- src/Academico/Principal.java
+++ src/Academico/Principal.java
@@ -4,14 +4,36 @@ import java.util.Scanner;
 
 public class Principal {
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
+		menuOpcoes();
+	}
 
+	private static void menuOpcoes() {
+		Scanner s = new Scanner(System.in);
+		String escolha = "";
+		System.out.println("===Opções===");
+		System.out.println("1 - Para inserir novo aluno");
+		System.out.println("Qualquer outro caracter para sair");
+		escolha = s.nextLine().toUpperCase();
+		switch (escolha) {
+		case ("1"):
+			inserirAluno();
+		default:
+			System.out.println("Fim");
+			break;
+		}
+
+	}
+
+	public static void inserirAluno() {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Inserir nova nota para um aluno");
 		System.out.println("Digite o nome do aluno: ");
-		Aluno aluno = new Aluno(s.nextLine());
+		String nome = s.nextLine();
+		Aluno aluno = new Aluno(nome);
 		System.out.println("Digite a sua primeira nota " + aluno.getNome() + ": ");
 
 		while (aluno.getNota1() < 0 || aluno.getNota1() > 10) {
-			aluno.setNota1(s.nextFloat());
+			aluno.setNota1(Float.parseFloat(s.nextLine()));
 			if (aluno.getNota1() < 0 || aluno.getNota1() > 10) {
 				System.out.println("A nota nao pode ser menor que 0 ou maior que 10 ");
 				System.out.println("Digite a nota novamente ");
@@ -27,6 +49,7 @@ public class Principal {
 			}
 		}
 		aluno.imprimirResultado();
+		menuOpcoes();
 	}
 
 }
